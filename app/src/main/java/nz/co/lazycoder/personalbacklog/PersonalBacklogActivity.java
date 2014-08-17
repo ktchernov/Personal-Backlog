d07b97f389883d802dc5a692b6368854aab927bf
ktchernov <k.tchernov@gmail.com>
2014-08-17 19:59:37 +1200

    Added activity and fragment with an in-progress list and a backlog. Can add items to backlog and transfer items from in-progress to backlog.

---
+++ app/src/main/java/nz/co/lazycoder/personalbacklog/PersonalBacklogActivity.java
@@ -0,0 +1,15 @@
+package nz.co.lazycoder.personalbacklog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PersonalBacklogActivity extends Activity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_backlog);
+
+    }
+}
