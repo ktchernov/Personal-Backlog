94ecb5d8d0f6e603b8b38fcd757d9c25de7fc410
ktchernov <k.tchernov@gmail.com>
2014-10-18 22:15:31 +1300

    Switch to compat Material Theme instead of Holo.

--- app/src/main/java/nz/co/lazycoder/personalbacklog/view/PersonalBacklogActivity.java
+++ app/src/main/java/nz/co/lazycoder/personalbacklog/view/PersonalBacklogActivity.java
@@ -2,11 +2,12 @@ package nz.co.lazycoder.personalbacklog.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 import nz.co.lazycoder.personalbacklog.R;
 
 
-public class PersonalBacklogActivity extends Activity  {
+public class PersonalBacklogActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
